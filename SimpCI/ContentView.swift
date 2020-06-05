ee0839d6baa91c13cd4c8157650913b6401aaca9
Peter <pourangshakib@hotmail.com>
2020-06-05 09:26:18 +0430

    iOS: Started

---
+++ SimpCI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpCI
+//
+//  Created by Xcode Server on 6/5/20.
+//  Copyright © 2020 Xcode Server. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
